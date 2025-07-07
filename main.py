5f0766db92f0da04d824a3f5309855ec3d15d469
Rishu <sheth.rishu50@gmail.com>
2025-07-07 20:16:24 +0530

    URL

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import os
 
 load_dotenv()
 app = Flask(__name__)
-CORS(app, origins=["http://localhost:5173"], supports_credentials=True)
+CORS(app, origins=["https://green-spoon.vercel.app"], supports_credentials=True)
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_ANON_KEY")
